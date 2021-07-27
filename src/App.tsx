e035ee57cba0f4e485f086bc46b7b02d5b7117ab
makotot <makoto.ttn@gmail.com>
2021-07-28 02:54:33 +0900

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react'
-import { themeClass, exampleStyle } from './styles.css';
-
+import { themeClass, exampleStyle } from './styles.css'
 
 function App() {
   return (
     <div className={themeClass}>
-      <div className={exampleStyle}>
-        app
-      </div>
+      <div className={exampleStyle}>app</div>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), vanillaExtractPlugin()]
+  plugins: [vanillaExtractPlugin(), eslintPlugin(), reactRefresh()],
 })
