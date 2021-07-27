3374d13add9e1c0c86e5f97e1cda7c11ad19c5c0
makotot <makoto.ttn@gmail.com>
2021-07-28 01:43:34 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { themeClass, exampleStyle } from './styles.css';
+
+
+function App() {
+  return (
+    <div className={themeClass}>
+      <div className={exampleStyle}>
+        app
+      </div>
+    </div>
+  )
+}
+
+export default App
